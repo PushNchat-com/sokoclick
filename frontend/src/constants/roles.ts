ff3c9ae20f9932f8300c6fc93c74dd22f10f2f92
mbehgil <gilmb@pushnshop.com>
2025-04-20 13:24:32 +0100

    Delete all files to rebuild project from scratch

--- frontend/src/constants/roles.ts
+++
@@ -1,9 +0,0 @@
-export type UserRole = 'buyer' | 'seller' | 'admin';
-
-export const USER_ROLES = {
-  BUYER: 'buyer' as UserRole,
-  SELLER: 'seller' as UserRole,
-  ADMIN: 'admin' as UserRole
-};
-
-export default USER_ROLES; 
